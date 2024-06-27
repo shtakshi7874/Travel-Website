a1ecaa58e1895c06841b382f31f96a838231e986
unknown <shtakshigarg43597@gmail.com>
2024-06-27 21:27:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+let searchBtn= document.querySelector('#search-btn');
+let searchBar= document.querySelector('search-bar-container');
+let formBtn= document.querySelector('#login-btn');
+let LoginForm= document.querySelector('.login-form-container');
+let FormClose= document.querySelector('#form-close');
+let menu=document.querySelector('#menu-bar');
+let navbar=document.querySelector('.navbar');
+let videoBtn=document.querySelectorAll('.vid-btn');
+
+
+window.onscroll=()=>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    LoginForm.classList.remove('active');
+}
+
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click',()=>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click',()=>{
+    LoginForm.classList.add('active');
+});
+
+FormClose.addEventListener('click',()=>{
+    LoginForm.classList.remove('active');
+});
+
+videoBtn.forEach(btn => {
+    btn.addEventListener('click',()=>{
+        $('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src=btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src=src;
+    })
+});
+
+var swiper=new Swiper(".review-slider",{
+    spaceBetween:20,
+    loop:true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        },
+    },
+});
